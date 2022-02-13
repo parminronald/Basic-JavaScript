3538dcf53ab11115120194e4807a8e16eb74b86e
parminronald <parmin.ronald@gmail.com>
2022-02-13 10:55:43 +0700

    Merubah nama hari

--- sanbox/04-switch-get-day.js
+++ sanbox/04-switch-get-day.js
@@ -1,29 +1,29 @@
 switch (new Date().getDay()) {
   case 0:
-    day = "Sunday";
+    day = "Minggu";
     console.log(day);
     break;
   case 1:
-    day = "Monday";
+    day = "Senin";
     console.log(day);
     break;
   case 2:
-    day = "Tuesday";
+    day = "Selasa";
     console.log(day);
     break;
   case 3:
-    day = "Wednesday";
+    day = "Rabu";
     console.log(day);
     break;
   case 4:
-    day = "Thursday";
+    day = "Kamis";
     console.log(day);
     break;
   case 5:
-    day = "Friday";
+    day = "Jumat";
     console.log(day);
     break;
   case 6:
-    day = "Saturday";
+    day = "Sabtu";
     console.log(day);
 }
